518a082aba9a2bba518ce2770c8e9b4be5f58748
Sven Siegmund <sven.siegmund@iav.de>
2021-06-14 13:48:16 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,50 @@
 
 from setuptools import setup
 from setuptools import find_packages
+import pathlib
+import re
 
 
+this_dir = pathlib.Path(__file__).parent.absolute()
 project_name = "ordered_argparse"
 package_dir = "src"
+path_to_init_file = this_dir / package_dir / project_name / "__init__.py"
 
-with open("README.md", "r", encoding="utf-8") as fh:
+
+with open("README.md", mode="r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+
+with open(this_dir / "requirements.txt", mode="r", encoding="utf-8") as reqs:
+    requirements = [line.strip() for line in reqs]
+
+
+def get_property(property: str, path_to_init_file: pathlib.Path) -> str:
+    """
+    Reads a property from the project's __init__.py
+    e.g. get_property("__version__") --> "1.2.3"
+    """
+    regex = re.compile(r"{}\s*=\s*[\"'](?P<value>[^\"']*)[\"']".format(property))
+    try:
+        with open(path_to_init_file) as initfh:
+            try:
+                result = regex.search(initfh.read()).group("value")
+            except AttributeError:
+                result = None
+    except FileNotFoundError:
+        result = None
+    return result
+
 
 setup(
         name=project_name,
-        version="1.0.4",
+        version=get_property("__version__", path_to_init_file),
         description="Modified version of argparse which remembers the order of CLI arguments",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        author="Sven Siegmund",
-        author_email="sven.siegmund@gmail.com",
-        url="https://github.com/Nagidal/ordered_argparse",
+        author=get_property("__author__", path_to_init_file),
+        author_email=get_property("__author_email__", path_to_init_file),
+        url=get_property("__url__", path_to_init_file),
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: End Users/Desktop",
--- src/ordered_argparse/ordered_argparse.py
+++ src/ordered_argparse/ordered_argparse.py
@@ -63,11 +63,7 @@ considered public as object names -- the API of the formatter objects is
 still considered an implementation detail.)
 """
 
-__version__ = "1.0.4"
-__author__ = "Sven Siemund"
-__author_email__ = "sven.siegmund@gmail.com"
-__maintainer__ = "Sven Siegmund"
-__url__ = "https://github.com/Nagidal/ordered_argparse"
+__version__ = "1.1"
 __all__ = [
     'ArgumentParser',
     'ArgumentError',
